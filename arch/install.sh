1f79f2fcbb4c0e404fb3e5c9b0b42d053a77b5ec
George Miles <gmiles83@gmail.com>
2020-08-21 21:19:07 -0400

    removed mirror ranking and allowed passwordless root

--- arch/install.sh
+++ arch/install.sh
@@ -69,12 +69,6 @@ mount $DISK"3" /mnt
 mkdir /mnt/boot
 mount $DISK"2" /mnt/boot
 
-# Only use fastest mirrors
-pacman --noconfirm -S pacman-contrib
-cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
-cat /etc/pacman.d/mirrorlist | grep "United States" -A 1 > /etc/pacman.d/mirrorlist.USA
-rankmirrors -n 6 /etc/pacman.d/mirrorlist.USA > /etc/pacman.d/mirrorlist
-
 # Install arch on new root partition
 pacstrap /mnt/ base base-devel linux mkinitcpio grub dhcpcd
 
@@ -96,5 +90,7 @@ arch-chroot /mnt mkinitcpio -p linux
 
 arch-chroot /mnt grub-install $DISK
 arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg
+
+arch-chroot /mnt passwd -d root
 
 arch-chroot /mnt systemctl enable dhcpcd.service
